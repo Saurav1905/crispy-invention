6e038c454187a3c4c8cb1013f79b61964d290491
Saurav <xauravdas01@gmail.com>
2022-06-30 01:38:59 +0530

    clean terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::{color, style, terminal_size};
+
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    println!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('q') => break,
+            _ => {}
+        }
+    }
 }
